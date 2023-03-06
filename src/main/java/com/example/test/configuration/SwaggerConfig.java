82fc78e114f2fde6f2d0e95b8befdfce334841d0
Osmolovskiy Yuriy <osmolovskiy.yuriy@ya.ru>
2023-03-06 13:39:36 +0300

    2 working security JWT conf - Swagger not working

--- src/main/java/com/example/test/configuration/SwaggerConfig.java
+++ src/main/java/com/example/test/configuration/SwaggerConfig.java
@@ -1,8 +1,11 @@
 package com.example.test.configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -14,15 +17,25 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI(@Value("${server.servlet.context-path}") String contextPath) {
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new io.swagger.v3.oas.models.security.SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+
                 .addServersItem(new Server().url(contextPath))
                 .info(new Info()
                         .title("Lashes")
                         .version("1.0.0")
                         .contact(new Contact()
-                                .email("Osmolovskiy.Yuriy@ya.ru")
-                                .url("https://github.com/bones-wp")
-                                .name("Osmolovkiy Yuriy")
+                                .url("https://github.com/intensiveproject/nogotochki")
+                                .name("Team Sunrise")
                         )
                 );
     }
---
+++ src/main/java/com/example/test/controller/HelloController.java
@@ -0,0 +1,33 @@
+package com.example.test.controller;
+
+import com.example.test.security.AuthService;
+import com.example.test.security.JwtAuthentication;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+@RequiredArgsConstructor
+public class HelloController {
+
+    private final AuthService authService;
+
+    @PreAuthorize("hasAuthority('USER')")
+    @GetMapping("hello/user")
+    public ResponseEntity<String> helloUser() {
+        final JwtAuthentication authInfo = authService.getAuthInfo();
+        return ResponseEntity.ok("Hello user " + authInfo.getPrincipal() + "!");
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @GetMapping("hello/admin")
+    public ResponseEntity<String> helloAdmin() {
+        final JwtAuthentication authInfo = authService.getAuthInfo();
+        return ResponseEntity.ok("Hello admin " + authInfo.getPrincipal() + "!");
+    }
+
+}
