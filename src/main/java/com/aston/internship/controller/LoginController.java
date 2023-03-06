96d434761d8e6db348ab176148a11d3e84a00383
Osmolovskiy Yuriy <osmolovskiy.yuriy@ya.ru>
2023-03-06 13:48:04 +0300

    1 Working security JWT conf - Swagger not working

--- src/main/java/com/aston/internship/controller/LoginController.java
+++ src/main/java/com/aston/internship/controller/LoginController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 import java.security.Principal;
 
 @RestController
-public class HomeController {
+public class LoginController {
 
-    @GetMapping(value = "/")
+    @GetMapping(value = "/login")
     public String home(Principal principal) {
         return "Hello, " + principal.getName();
     }
